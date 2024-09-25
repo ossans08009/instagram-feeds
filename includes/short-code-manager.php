8f3db20f85aeab0780f9fafae6125f2b3edda03c
y.ogawa <ossan.s08009@gmail.com>
2024-09-25 21:57:22 +0900

    スライダー大まかに完成

--- includes/short-code-manager.php
+++ includes/short-code-manager.php
@@ -18,7 +18,7 @@ function instagram_feed_carousel_shortcode( $attr ) {
     }
 
     // カルーセル用のHTML開始
-    $output = '<div class="instagram-feed-carousel">';
+    $output  = '<div class="instagram-feeds">';
     
     // 投稿をループして、アイキャッチ画像を表示
     while ($query->have_posts()) {
@@ -28,7 +28,11 @@ function instagram_feed_carousel_shortcode( $attr ) {
         $thumbnail_url = get_post_meta( get_the_ID(), '_instagram_feed_thumbnail_url', true );
         $permalink = get_post_meta( get_the_ID(), '_instagram_feed_permalink', true );
         
-        $output .= '<div><a tartget="_blank" href="' . $permalink . '"><img src="' . esc_url($thumbnail_url) . '" alt="' . esc_attr(get_the_title()) . '"></a></div>';
+        $output .= '<div class="instagram-feed">';
+        $output .= '<a target="_blank" href="' . $permalink . '">';
+        $output .= '<img src="' . esc_url($thumbnail_url) . '" alt="' . esc_attr(get_the_title()) . '" />';
+        $output .= '</a>';
+        $output .= '</div>';
     }
 
     // HTML終了
@@ -41,56 +45,29 @@ function instagram_feed_carousel_shortcode( $attr ) {
 }
 add_shortcode('instagram_feed_carousel', 'instagram_feed_carousel_shortcode');
 
-// Slick Sliderのスクリプトとスタイルの読み込み
-function enqueue_slick_slider_scripts() {
+// プラグインのCSSを読み込む関数
+function my_plugin_enqueue_styles() {
+    // slick-sliderのjsとcssを読み込み
     wp_enqueue_style('slick-slider-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css');
     wp_enqueue_style('slick-slider-theme-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css');
     wp_enqueue_script('slick-slider-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'), null, true);
     
-    // カルーセルの初期化スクリプトを追加
-    wp_add_inline_script('slick-slider-js', '
-        jQuery(document).ready(function($) {
-            $(".instagram-feed-carousel").slick({
-                dots: true,
-                infinite: false,
-                slidesToShow: 5,
-                slidesToScroll: 5,
-                autoplay: false,
-                arrows: true,
-                focusOnSelect: true,
-                variableWidth: true,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
-                        }
-                    },
-                        {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2,
-                            autoplay: true,
-                            autoplaySpeed: 2000,
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                            autoplay: true,
-                            autoplaySpeed: 2000,
-                        }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
-        });
-    ');
+    // プラグインディレクトリからCSSを読み込む
+    wp_enqueue_style(
+        'instagram-feeds-style', // CSSハンドル名
+        plugin_dir_url(__FILE__) . '../asset/css/style.css', // CSSのパス
+        array(), // 依存関係（なければ空の配列）
+        '1.0.0', // バージョン
+        'all' // メディア（全ての画面向け）
+    );
+
+    // プラグインディレクトリからJSを読み込む
+    wp_enqueue_script(
+        'instagram-feeds-script', // JSハンドル名
+        plugin_dir_url(__FILE__) . '../asset/js/carousel-slider.js', // パス
+        array(), // 依存関係（なければ空の配列）
+        '1.0.0', // バージョン
+        true, // 読み込み位置指定
+    );
 }
-add_action('wp_enqueue_scripts', 'enqueue_slick_slider_scripts');
+add_action('wp_enqueue_scripts', 'my_plugin_enqueue_styles');
